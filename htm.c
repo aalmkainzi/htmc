8ed6de399333f9ac57e0c51edc0ab7dd12d949cf
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2024-03-15 02:06:11 +0300

    made htm.c reincludable, and also can be used for HTMC_PREFIX

--- htm.c
+++ htm.c
@@ -1,5 +1,9 @@
-#include <stdarg.h>
 #include "htmc.h"
+
+#ifndef HTMC_C
+#define HTMC_C
+
+#include <stdarg.h>
 
 const char *htmc_tags[] = {
 "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "math", "menu", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "slot", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"
@@ -513,3 +517,5 @@ void htmc_gurantee_cap(char **buffer, size_t *cap, size_t new_cap)
         *cap = new_cap;
     }
 }
+
+#endif /* HTMC_C */
